09f1901914e2abf18d85d2c21063b8b536417df5
Luca Piccirillo <luca.piccirillo@gmail.com>
2019-06-22 12:43:30 +0200

    Board subtargets + cleanup

---
+++ target/linux/brcm63xx-tch/VBNTK/target.mk
@@ -0,0 +1,11 @@
+SUBTARGET:=VBNTK
+BOARDNAME:=VBNT-K
+
+BRCM_CHIP:=63138
+ARCH:=arm
+CPU_TYPE:=cortex-a9
+CPU_SUBTYPE:=neon
+
+define Target/Description
+	Broadcom 63x38 (VBNT-K)
+endef
---
+++ target/linux/brcm63xx-tch/VBNTO/target.mk
@@ -0,0 +1,11 @@
+SUBTARGET:=VBNTO
+BOARDNAME:=VBNT-O
+
+BRCM_CHIP:=63138
+ARCH:=arm
+CPU_TYPE:=cortex-a9
+CPU_SUBTYPE:=neon
+
+define Target/Description
+	Broadcom 63x38 (VBNT-O)
+endef
